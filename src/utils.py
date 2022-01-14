8217d001d6a56da80aeef62eaa4b35ad2d939aee
luccle <lcleopark99@gmail.com>
2022-01-14 14:08:52 -0300

    romance gender added | function to upload new documents

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import os
+import requests
 
 def export_faq_csv(df_out, name):
 
@@ -8,3 +9,21 @@ def export_faq_csv(df_out, name):
         os.makedirs(path)
 
     df_out.to_csv(f'{path}/{name}_faq.csv')
+
+
+def post_files(gender):
+
+    url = "https://askfrank.ai:5000/update_faq"
+
+    payload={}
+    files=[
+    ("file",(gender["file_name"],open(f"./data/{gender['file_name']}","rb"),"text/csv"))
+    ]
+    headers = {
+    'auth_code': gender["auth_code"],
+    'x-access-token': gender["token"]
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload, files=files)
+
+    print(response.text)
